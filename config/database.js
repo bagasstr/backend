e92d908820c04a16deb3638f19e27167ffa10510
bagasstr <bagassatrio2805@gmail.com>
2023-07-23 18:10:43 +0700

    first commit

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // console.log(dotenv);
+// const connectionUri =
+//   "mysql://uzky7huydwuqswoh:mMH6Uhmu1124pXOo8KwK@b9q2prkirvnugxpcwefq-mysql.services.clever-cloud.com:3306/b9q2prkirvnugxpcwefq";
 
 const koneksi = new Sequelize(
   process.env.DB_DATABASE,
@@ -11,8 +13,18 @@ const koneksi = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
-    dialectModule: "mysql",
+    // dialectModule: "mysql",
   }
 );
+koneksi
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database:", err);
+  });
+
+// Call the testConnection function to check if the database connection is successful
 
 export default koneksi;
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
-// import koneksi from "./config/database.js";
+import koneksi from "./config/database.js";
 import Product from "./models/productModels.js";
 import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
-const port = process.env.DB_PORT || 5050;
+const port = process.env.DB_PORT;
 
 // (async () => {
-//   await Product.sync();
+//   try {
+//     await Product.sync();
+//   } catch (error) {
+//     console.error(error);
+//   }
 // })();
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +19,4 @@ app.use(express.json());
 
 app.use("/", productRoutes);
 
-app.listen(port, () => console.log(`Server running on ${port}`));
+app.listen(3306, () => console.log(`Server running on `));
