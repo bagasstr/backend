046615e865c79fcd32ab95e19b1505156aa5f4d5
bagasstr <bagassatrio2805@gmail.com>
2023-07-24 01:48:47 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 // import koneksi from "./config/database.js";
 // import Product from "./models/productModels.js";
+import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
@@ -18,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5174/", // Atur domain yang diizinkan
+    origin: "http://localhost:5174", // Atur domain yang diizinkan
     methods: "GET,PUT,POST,DELETE", // Atur metode HTTP yang diizinkan
     optionsSuccessStatus: 200, // Atur kode status untuk opsi pra-penerbangan (Preflight)
     credentials: true, // Izinkan pengiriman kredensial (cookie, header otentikasi, dll.)
