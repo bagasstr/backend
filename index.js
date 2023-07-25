0c2d6b4136e56d74979161269f0aad11f2e79a0c
bagasstr <bagassatrio2805@gmail.com>
2023-07-26 02:06:05 +0700

    cors fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 // import koneksi from "./config/database.js";
 // import Product from "./models/productModels.js";
-import cors from "cors";
+// import cors from "cors";
 import productRoutes from "./routes/productRoutes.js";
-import handleCors from "./middleware/productMiddle.js";
+import { handleCors } from "./middleware/productMiddle.js";
 
 const app = express();
 const port = process.env.DB_PORT;
