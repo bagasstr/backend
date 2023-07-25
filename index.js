4a9eb928d4df805c91b8846d2d751ffd85ff2bf4
bagasstr <bagassatrio2805@gmail.com>
2023-07-26 00:06:11 +0700

    cors

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "", // Atur domain yang diizinkan
-    methods: "GET, PUT, POST, DELETE", // Atur metode HTTP yang diizinkan
+    origin: "*", // Atur domain yang diizinkan
+    methods: [GET, PUT, POST, DELETE], // Atur metode HTTP yang diizinkan
     optionsSuccessStatus: 200, // Atur kode status untuk opsi pra-penerbangan (Preflight)
     credentials: false, // Izinkan pengiriman kredensial (cookie, header otentikasi, dll.)
   })
